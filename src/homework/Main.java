bf24c5e74ccc6eb8b4b73847d16b75c92b3406a9
miksor2023 <miksor@yandex.ru>
2023-09-27 15:54:48 +0300

    homework branch

--- src/homework/Main.java
+++ src/homework/Main.java
@@ -1,49 +1,15 @@
 package homework;
 
 public class Main {
-    public static void main() {
-
-    }
-    public class Book {
-        //объявляем поля класса Воок
-        private String title;
-        private Author author;
-        private int publishingYear;
-        //создаём конструктор класса Book
-        public Book (String title, Author author, int publishingYear) {
-            this.title = title;
-            this.author = author;
-            this.publishingYear = publishingYear;
-        }
-        //геттер поля title (название)
-        public String getTitle() {
-            return title;
-        }
-        //геттер поля author (автор)
-        public Author getAuthor() {
-            return author;
-        }
-        //геттер поля publoshingYear (год издания)
-        public int getPublishingYear() {
-            return publishingYear;
-        }
-        //сеттер поля publoshingYear (год издания) с проверкой валидности
-        public void setPublishingYear(int publishingYear) {
-            if (publishingYear < 1950 || publishingYear > 2023) {
-                System.out.println("Некорректный год издания:" + publishingYear);
-                return;
-            }
-            this.publishingYear = publishingYear;
-        }
-    }
-    public class Author {
-        //объявляем поля класса Author
-        private String name;
-        private String secondName;
-        //создаём конструктор класса Author
-        public Author (String name, String secondName) {
-            this.name = name;
-            this.secondName = secondName;
-        }
+    public static void main(String[] args) {
+        Author bulgakov = new Author("Михаил", "Булгаков");
+        Author pushkin = new Author("Александр", "Пушкин");
+        Book masterIMargarita = new Book("Мастер и Маргарита", bulgakov, 1997);
+        Book poltava = new Book("Полтава", pushkin, 1986);
+        System.out.println(masterIMargarita.getAuthor() + ", " + masterIMargarita.getTitle() + ", " + masterIMargarita.getPublishingYear());
+        System.out.println(pushkin.getName() + " " + pushkin.getSecondName());
+        masterIMargarita.setPublishingYear(2000);
+        System.out.println(masterIMargarita.getPublishingYear());
     }
 }
+
